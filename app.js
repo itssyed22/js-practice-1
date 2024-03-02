57df2669d39bf6bec6c31cf35eef6750b3d4a505
Abdullah <you@example.com>
2024-03-02 18:47:30 +0500

    practice on js

---
+++ app.js
@@ -0,0 +1,44 @@
+// var userInput = prompt("Enter a number:")
+// if (!isNaN(userInput)) {
+//     alert("Input is a number")
+// } else {
+//     alert("Input is not a number")
+// }
+
+// var num = prompt("Enter a number:")
+// if(num%2 == 0){
+//     alert("The number is even")
+// }else{
+//     alert("The number is odd")
+// }
+
+// var operator = prompt("Which type of calculation you want please enter + , - , * , / , %")
+// var firstValue = prompt("Enter first number")
+// var secondValue = prompt("Enter second number")
+// var add = parseInt(firstValue) + parseInt(secondValue)
+// var sub = parseInt(firstValue) - parseInt(secondValue)
+// var mult = parseInt(firstValue) * parseInt(secondValue)
+// var divd = parseInt(firstValue) / parseInt(secondValue)
+// var mod = parseInt(firstValue) % parseInt(secondValue)
+
+// if(operator == "+"){
+//     alert(add)
+// }
+// if(operator == "-"){
+//     alert(sub)
+// }
+// if(operator == "*"){
+//     alert(mult)
+// }
+// if(operator == "/"){
+//     alert(divd)
+// }
+// if(operator == "%"){
+//     alert(mod)
+// }
+// else{"invalid input"}
+
+// var greetinMessage = "Hello how are you"
+// var yourName = prompt("Enter your name")
+// var greeted = greetinMessage + " " + yourName
+// alert(greeted)
